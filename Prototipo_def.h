5714dd87283fd0516326b132376f050bcb33bb82
S4UL0 <90044415+saulooficinas@users.noreply.github.com>
2021-10-26 19:43:04 -0300

    Update Prototipo_def.h

--- Prototipo_def.h
+++ Prototipo_def.h
@@ -7,27 +7,18 @@
 //Valor de interações do filtro de média móvel
 #define max_int 100
 
+//Pino para configurar WiFi e interrupção do sensor de referência.
+#define pin_ISR_WiFi 36
+#define pin_ISR_Ref 37
+
 //define saídas
 #define PIN_PROTOTIPO 32
 #define PIN_REF 33
 #define IP_SENSOR 1
 
-//Defines do OLED
-#define OLED_ADDR 0x3c
 
-//Define das linhas
-#define OLED_LINE1 0
-#define OLED_LINE2 10
-#define OLED_LINE3 208
-#define OLED_LINE4 30
-#define OLED_LINE5 40
-
-//define tamanhos
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-
-//Endereço host 
-const char* host="192.168.0.108";
+//Endereço host
+const char* host = "192.168.0.107";
 
 //Chars improtantes
 char CharS[] = "iniciando saidas...";
@@ -36,14 +27,14 @@ char CharW[] = "iniciando WiFi...";
 char CharF[] = "iniciando FreeRTOS...";
 
 //Chars para WiFiManager
-char ssidWM[]="SWMF_AP";
-char senhaWM[]="12345678";
+char ssidWM[] = "SMWF_AP";
+char senhaWM[] = "12345678";
 
 //Criando estrutura para enviar situação do protótipo e o sensor de referência
 struct sensorRef
 {
   //Endereço dos pinos
-  int pino[2];
+  uint8_t pino[2];
 
   //Valor encontrado neste pino
   float valor[2];
@@ -60,10 +51,14 @@ void printDisplay(float a, float b);
 void initSaidas();
 void initWiFi();
 void creditsProto();
-void barDisplay(int a, int b, char* v);
+void initBarDisplay(int a, int b, char* v);
+void errorMessage(uint8_t erroTxt, uint8_t TimeDelay);
 
 //Funções de retorno float
 float readPrototipo();
 float readRefSensor();
 float transUnit(float d);
 float difValues(float a, float b);
+
+//Funções de chamada das ISR
+void WiFiISRCallback();
