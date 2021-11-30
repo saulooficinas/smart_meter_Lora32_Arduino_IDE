2e55991937b8a3cdfa4a64925f7c16f32770ae29
S4UL0 <90044415+saulooficinas@users.noreply.github.com>
2021-11-30 15:41:00 -0300

    Update Projeto_Smart_Meter_V2.ino

--- Projeto_Smart_Meter_V2.ino
+++ Projeto_Smart_Meter_V2.ino
@@ -1,12 +1,12 @@
 /********************************************************************************************
-   PROGRAMA PRINCIPAL DO SMWF ( Smart Meter Water Flux) - OFICINAS 4.0 - Versão 1.2
+   PROGRAMA PRINCIPAL DO SMWF ( Smart Meter Water Flux) - OFICINAS 4.0 - Versão 1.4
    Instituição: Instituto Federal de Alagoas - Campus Palmeira dos índios
 
    Descrição: Ele irá fazer a leitura de eletrodos no fluído
    e irá enviar a vazão para um servidor WEB. Além disso, também receberá dados
    via Bluetooth e terá opções de auto-teste para avaliar seu funcionamento.
 
-   Ultima atualização: 17/11/2021 (SAULO JOSÉ ALMEIDA SILVA)
+   Ultima atualização: 30/11/2021 (SAULO JOSÉ ALMEIDA SILVA)
  ********************************************************************************************/
 
 
@@ -22,12 +22,14 @@
   |----------------|----------------|-------------------|------------------------------------------|
   |-vTaskWiFiReset |      00        |         04        | Chama a rotina de reconfigurar WiFi      |
   |----------------|----------------|-------------------|------------------------------------------|
+  |-vTaskBobinas   |      00        |         01        | Efetua operações de transição nas bobinas|
 ****************************************************************************************************
 
 
+
     >> NÚCLEOS:
-        => APP_CPU_NUM = 00 
-        => PRO_CPU_NUM = 01 
+        => APP_CPU_NUM = 00
+        => PRO_CPU_NUM = 01
 
     >> Fórmula utilizada no Protótipo:
      V = U.(pi.D)/(4.k.B)
@@ -41,7 +43,7 @@
 
     BUG ATUAL:
     - Leitura do analogRead(PIN_PROTOTIPO) está fixado em um valor 4095 ( )
-    - Ao mantar a interrupção, o código nem inicia. ( ) 
+    - Ao mantar a interrupção, o código nem inicia. ( )
     - Task do Reset WiFi está com problema de sintexe. ( )
 
     Necessidades:
@@ -53,6 +55,9 @@
 //Bibliotecas para utilizar o display OLED
 #include "Arduino.h"
 #include "heltec.h"
+
+//Biblioteca de controle PWM
+#include "driver/mcpwm.h"
 
 //Header do protótipo.
 #include "Prototipo_def.h"
@@ -84,6 +89,7 @@ TaskHandle_t vTaskDataSensorHandle;
 TaskHandle_t vTaskDisplayHandle;
 TaskHandle_t vTaskMySQLHandle;
 TaskHandle_t vTaskWiFiResetHandle;
+TaskHandle_t vTaskBobinasHandle;
 
 //Semaforos Handles
 SemaphoreHandle_t WiFiResetSemaphore;
@@ -93,6 +99,7 @@ void vTaskDataSensor(void* pvParamaters);
 void vTaskDisplay(void* pvParamaters);
 void vTaskMySQL(void* pvParamaters);
 void vTaskWiFiReset(void* pvParamaters);
+void vTaskBobinas(void* pvParamaters);
 
 //Função para callback da configuração AcessPoint
 void configModeCallback(WiFiManager *myWiFiManager);
@@ -129,7 +136,7 @@ void IRAM_ATTR WiFiISRCallback()
 void setup() {
   //Iniciando Serial
   Serial.begin(115200);
-  
+
   //Gerando interrupções
   //attachInterrupt(digitalPinToInterrupt(pin_ISR_WiFi), WiFiISRCallback, RISING);
 
@@ -138,8 +145,9 @@ void setup() {
   initDisplay(); //Inicializa Display OLED
   initSaidas(); //Configura saídas
   initWiFi(); //Inicia comunicação WiFi  initFreeRTOS(); //Inicia funções do FreeRTOS
+  initPWMbobinas(); //Configura controle das bobinas;
+  initFreeRTOS(); //Inicia Tarefas.
   Serial.println("[SMWF]: Configurações finalizadas!\n\n");
-
 }
 /*======================================|| loop ||==============================================*/
 void loop() {
@@ -150,7 +158,7 @@ void loop() {
 void initFreeRTOS()
 {
   Serial.println("[SMWF]: Iniciando FreeRTOS.");
-  BaseType_t returnSensor, returnDisplay, returnMySQL, returnWiFiReset;
+  BaseType_t returnSensor, returnDisplay, returnMySQL, returnWiFiReset, returnBobina;
 
   //Gerando filas
   xFilaDisplay = xQueueCreate(
@@ -219,10 +227,26 @@ void initFreeRTOS()
                         , NULL //Parâmetro inicial enviado
                         , 4 //Prioridade
                         , &vTaskWiFiResetHandle //Handle da tarefa
-                        , 0); //Utilizará o segundo núcleo de processamento.
+                        , 0); //Utilizará o primeiro núcleo de processamento.
     if (returnWiFiReset != pdTRUE)
     {
       Serial.println("[SMWF]: Erro 1.  Não foi possível gerar a tarefa do WiFiReset.");
+      errorMessage(666, 5000);
+      while (1);
+    }
+
+    returnBobina = xTaskCreatePinnedToCore(
+                     vTaskBobinas //Tarefa
+                     , "Controle Bobinas" //Nome para debug
+                     , configMINIMAL_STACK_SIZE + 1024 //Memória utilziada
+                     , NULL // Variável de inicialização
+                     , 1 //Prioridade
+                     , &vTaskBobinasHandle // Handle da tarefa
+                     , 0); //Utiliza o primeiro núcleo de processamento
+
+    if (returnBobina != pdTRUE)
+    {
+      Serial.println("[SMWF]: Erro 1.  Não foi possível gerar a tarefa do vTaskBobina..");
       errorMessage(666, 5000);
       while (1);
     }
@@ -247,14 +271,14 @@ void vTaskDataSensor(void* pvParamaters)
 
   //Fazendo tratamento dos dados
   float dados[max_int], acc, media;
-  
+
   while (1)
   {
     acc = 0;
     //Limpa todos os dados anteriores.
     for (int i = 0; i < max_int; i++)
     {
-      dados[i] = analogRead(PIN_PROTOTIPO) * 6.1/160;
+      dados[i] = analogRead(PIN_PROTOTIPO) * 6.1 / 160;
       //Serial.println("Valor do dados [" + Streing(i) + "]:" + String(dados[i]));
       acc += dados[i];
     }
@@ -379,7 +403,7 @@ void vTaskMySQL(void* pvParamaters)
         {
           Serial.println();
           Serial.println("[MySQL_T]: Houve um erro no servidor e dados errados foram encaminhados.");
-          errorMessage(411,5000);
+          errorMessage(411, 5000);
         }
       }
       Serial.println();
@@ -418,6 +442,36 @@ void vTaskWiFiReset(void* pvParamaters)
     }
   }
 }
+
+//Tarefa de controle das bobinas.
+void vTaskBobinas(void* pvParamaters)
+{
+  while (1)
+  {
+    bobina_ascendente(MCPWM_UNIT_0, MCPWM_TIMER_0, 100);//Gira o motor no sentido horário
+    vTaskDelay(pdMS_TO_TICKS(3000));
+
+    bobina_descendente(MCPWM_UNIT_0, MCPWM_TIMER_0, 100);
+    vTaskDelay(pdMS_TO_TICKS(3000));
+
+    bobina_desligada(MCPWM_UNIT_0, MCPWM_TIMER_0);//Para o motor.
+    vTaskDelay(pdMS_TO_TICKS(2000));
+
+    for (int i = 10; i <= 100; i++)
+    {
+      bobina_ascendente(MCPWM_UNIT_0, MCPWM_TIMER_0, i); //define ciclo de trabalho usando i
+      vTaskDelay(pdMS_TO_TICKS(200));
+    }
+
+    for (int i = 100; i >= 10; i--)
+    {
+      bobina_ascendente(MCPWM_UNIT_0, MCPWM_TIMER_0, i); //define ciclo de trabalho usando i
+      vTaskDelay(pdMS_TO_TICKS(200));
+    }
+    vTaskDelay(1);
+  }
+}
+
 
 /* Definição das demais funções*/
 //Procedimentos
@@ -493,6 +547,7 @@ void initWiFi()
   wifiManager.autoConnect("SMWF_AP", "12345678");
 }
 
+
 //Callback para WiFiManager
 void configModeCallback(WiFiManager * myWiFiManager)
 {
@@ -530,8 +585,35 @@ float readPrototipo()
 {
   double value;
 
-  //Obs: Seria 25/1023.0 caso fosse um Arduino
-  value = analogRead(PIN_PROTOTIPO) * (5) / (1023.0);
+  /*
+    Foi aplicado um divisor de tensão em que Vout= Vin*2/3, assim, alimento o sensor com 5V, e posso pegar tensões de 0 a 25V
+    Como o sensor tem fator Vout=Vin/5, a tensão real (Vr) com o Vout pode ser encontrado assim:
+
+    Vout = 2.Vr/(3*5) = 2.Vr/15
+
+    Como a tensão associada é com 12 bits de resolução. A tensão mínima captada pelo ESP32 é de;
+
+    Resolução = 3,3 V /2^12 ~= 0.8057 mV
+
+    Para isso, é necessário uma tensão de, no mínimo 6,043 mV nos eletrodos.
+
+    3,3 V ----> 4095
+    Vout -----> Sinal
+
+    Vout = 2Vr/15
+
+    3,3V ---> 4095
+    2Vr/15 ---> Sinal
+
+    Vr.2.4095/15 = Sinal*3.3 => Vr = 3.3*15*Sinal/(2*4095) = Sinal * 3.3 / 546;
+
+    Vmin(Medição) = 6.043 mV
+
+    V(real)=Sinal.10/(1638)
+
+    => Adicionar na documentação.
+  */
+  value = analogRead(PIN_PROTOTIPO) * 10 / 1638;
 
   return value;
 }
@@ -586,4 +668,62 @@ void errorMessage(int erroTxt, uint8_t TimeDelay)
   Heltec.display->drawString(37, 54, "Error " + String(erroTxt));
   Heltec.display->display();
   delay(TimeDelay);
+}
+
+//Inicia as configurações do controle PWM
+void initPWMbobinas()
+{
+  Serial.println("[SMWF]: Iniciando automação PWM");
+  //mcpwm_gpio_init(unidade PWM 0, saida A, porta GPIO) => Instancia o MCPWMA para a porta GPIO_PWM0A_OUT
+  mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, GPIO_PWM0A_OUT);
+
+  //mcpwm_gpio_init(unidade PWM 0, saida b, porta GPIO) => Instancia o MCPWMA para a porta GPIO_PWM0B_OUT
+  mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0B, GPIO_PWM0B_OUT);
+
+  //Preparando variável com configuração para PWM
+  mcpwm_config_t pwm_config;
+
+  pwm_config.frequency = 1000; //frequência = 1000 hz
+  pwm_config.cmpr_a = 0; //ciclo de trabalho (duty cycle) do PWMxA =0 de 0 a 100%
+  pwm_config.cmpr_b = 0; //Ciclo de trabalho (duty cycle) do PWMxB=0
+  pwm_config.counter_mode = MCPWM_UP_COUNTER; // Para MCPWM assimétrico
+  pwm_config.duty_mode = MCPWM_DUTY_MODE_0; // Define ciclo de trabalho em nível alto
+
+  //Inicia(Unidade 0, Timer 0, Configuração PWM)
+  mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);
+  Serial.println("[SMWF]: Finalizando automação PWM");
+}
+
+static void bobina_ascendente(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num, float duty_cicle)
+{
+  //mcpwm_set_signal_low(unidade PWM(O ou 1), número do timer (0,1 ou 2), saída (A ou B));
+  mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_B);//Desliga o sinal do MCPWM no Operaor B (Define o sinal em Baixo)
+
+  //mcpwm_set_duty(unidade PWM(0 ou 1), Número do timer (0, 1 ou 2), Saída (A ou B),  Ciclo de trabalho (% do PWM));
+  mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_A, duty_cicle);//Configura a porcentagem do PWM no Operador A (Ciclo de trabalho)
+
+  //mcpwm_set_duty_type(unidade PWM(0 ou 1), Número do Timer (0,1 ou 2), Operador (A ou B), Nível do ciclo de trabalho));
+  mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_A, MCPWM_DUTY_MODE_0);//define o ciclo de trabalho (alto ou baixo)
+
+  //Nota: chame essa função toda vez que for chamado "mcpwm_set_signal_low" ou "mcpwm_set_signal_high"
+  //para manter o ciclo de trabalho configurado anteriormente.
+}
+static void bobina_descendente(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num, float duty_cicle)
+{
+  //mcpwm_set_signal_low(unidade PWM(O ou 1), número do timer (0,1 ou 2), saída (A ou B));
+  mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_A);//Desliga o sinal do MCPWM no Operaor B (Define o sinal em Baixo)
+
+  //mcpwm_set_duty(unidade PWM(0 ou 1), Número do timer (0, 1 ou 2), Saída (A ou B),  Ciclo de trabalho (% do PWM));
+  mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_B, duty_cicle);//Configura a porcentagem do PWM no Operador A (Ciclo de trabalho)
+
+  //mcpwm_set_duty_type(
+  mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_B, MCPWM_DUTY_MODE_0);//define o ciclo de trabalho (alto ou baixo)
+
+  //Nota: chame essa função toda vez que for chamado "mcpwm_set_signal_low" ou "mcpwm_set_signal_high"
+  //para manter o ciclo de trabalho configurado anteriormente.
+}
+static void bobina_desligada(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num)
+{
+  mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_A);//Desliga o sinal no operador A
+  mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_B);//Desliga o sinal no operador B
 }
